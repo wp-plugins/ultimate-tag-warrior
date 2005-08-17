99bacd0ea2af4179d192b465b13c43498e7b2955
christined <christined@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-08-17 09:36:19 +0000

    An external javascript file for the Ajaxy bits
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-tag-warrior/trunk@3058 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins/UltimateTagWarrior/ultimate-tag-warrior-ajax-js.php
@@ -0,0 +1,31 @@
+<?php $ajaxurl = $_GET['ajaxurl'] ?>
+function createRequestObject() {
+    var ro;
+    var browser = navigator.appName;
+    if(browser == "Microsoft Internet Explorer"){
+        ro = new ActiveXObject("Microsoft.XMLHTTP");
+    }else{
+        ro = new XMLHttpRequest();
+    }
+    return ro;
+}
+
+var http = createRequestObject();
+
+function sndReq(action, tag, post, format) {
+    http.open('get', '<?= $ajaxurl ?>?action='+action+'&tag='+tag+'&post='+post+'&format='+format);
+    http.onreadystatechange = handleResponse;
+    http.send(null);
+}
+
+function handleResponse() {
+    if(http.readyState == 4){
+        var response = http.responseText;
+        var update = new Array();
+
+        if(response.indexOf('|' != -1)) {
+            update = response.split('|');
+            document.getElementById("tags-" + update[0]).innerHTML = update[1];
+        }
+    }
+}
